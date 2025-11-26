c68b3e786d3e90cae10247e77a2217135675c743
mesuriyavivek7 <vivekmesuriya6@gmail.com>
2025-11-27 00:12:45 +0530

    Update dependencies and middleware configuration; remove unused page component

---
+++ src/app/site/page.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className=''>
+      <section className='h-full w-full pt-36 relative flex items-center justify-center flex-col'>
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+      <p className='text-center'>Run your agency, in one place</p>
+      <div className='bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative'>
+        <h1 className='text-9xl font-bold text-center md:text-[250px]'>Buildo</h1>
+      </div>
+      <div className='flex justify-center items-center relative md:mt-[-70px]'>
+        <Image 
+        src={'/assets/preview.png'}
+        alt='preview'
+        width={1200}
+        height={1200}
+        className='rounded-tl-2xl rounded-tr-2xl border border-muted'
+        ></Image>
+      </div>
+      </section>
+    </main>
+  )
+}
